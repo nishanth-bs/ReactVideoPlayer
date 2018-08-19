6cdfc152163c702b731cb93ebe7e25c892b9da9a
Nishanth BS <nishanth.star1@gmail.com>
2018-08-19 13:48:00 +0530

    Add: fetch data from youtube

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
-
 const API_KEY = 'AIzaSyDG9qqcssfIk-1Z_eg1N9_GZYYi4FrsOOU';
-const App = ()=>{
-  return(
-      <div>
-        <SearchBar/>
-      </div>
-  )
+
+YTSearch({key:API_KEY, term :'RSS'}, function(data){
+  console.log(data);
+});
+
+class App extends Component{
+  render(){
+    return(
+        <div>
+          <SearchBar/>
+        </div>
+    );
+  }
+
 }
+
+
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
