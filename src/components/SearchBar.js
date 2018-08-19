afd6c7f334e7e6ca8069f4656f9d484dadc1fb50
Nishanth BS <nishanth.star1@gmail.com>
2018-08-19 13:09:01 +0530

    Add: Handling user event

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,8 +2,17 @@ import  React, {Component} from 'react';
 
 class SearchBar extends Component{
   render(){
-    return <input/>;
+    return(
+      <div>
+        <input onChange= {event => console.log(event.target.value)}/>
+        <input onChange = {this.onIpChange} />
+      </div>
+    );
   }
+  onIpChange(event){
+    console.log(event.target.value);
+  }
+
 }
 
 export default SearchBar;
